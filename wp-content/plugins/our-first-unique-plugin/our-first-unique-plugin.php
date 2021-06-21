9e66a3320abb9cfff6cb09bdabbae2767280854e
ArneSaknussemm <franciscomarconi@gmail.com>
2021-06-21 01:09:20 -0400

    Clase para el Plugin

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -6,3 +6,30 @@
 	Author: Arne Saknussemm
 	Author URI: https://web-pyme.cl
 */
+
+/**
+ * Miclase linda
+ */
+class RevisarTexto
+{
+	function __construct()
+	{
+		add_action( 'admin_menu', array( $this , 'menu_ajustes'));
+	}
+	function menu_ajustes()
+	{
+		add_options_page( 'Ajustes Revisión de Texto', 'Revisar texto', 'manage_options', 'revisar-texto', array($this, 'ajustes_html') );
+	}
+	function ajustes_html()
+	{
+		ob_start();
+		?>
+		<div class="wrap">
+			<h1>Hola</h1>
+		</div>
+		<?php
+		echo ob_get_clean();
+	}
+}
+
+$revisar_texto = new RevisarTexto();

2e8f771dd15bc53691a6961e0f4bdad71685610e
ArneSaknussemm <franciscomarconi@gmail.com>
2021-06-21 02:31:17 -0400

    Página de ajustes con un campo

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -1,11 +1,11 @@
 <?php
-/*
-	Plugin Name: Our Test Plugin
-	Description: A ver si result, jajaj
-	Version: 1.0
-	Author: Arne Saknussemm
-	Author URI: https://web-pyme.cl
-*/
+/**
+ *	Plugin Name: Our Test Plugin
+ *	Description: A ver si result, jajaj
+ *	Version: 1.0
+ *	Author: Arne Saknussemm
+ *	Author URI: https://github.com/ArneSaknussemm
+ */
 
 /**
  * Miclase linda
@@ -14,21 +14,51 @@ class RevisarTexto
 {
 	function __construct()
 	{
-		add_action( 'admin_menu', array( $this , 'menu_ajustes'));
+		add_action( 'admin_menu', array( $this, 'rvt_menu') );
+		add_action( 'admin_init', array( $this, 'rvt_ajustes') );
 	}
-	function menu_ajustes()
+	function rvt_ajustes()
 	{
-		add_options_page( 'Ajustes Revisión de Texto', 'Revisar texto', 'manage_options', 'revisar-texto', array($this, 'ajustes_html') );
+		add_settings_section( 'rvt_seccion_a', null, null, 'revisar-texto' );
+		add_settings_field( 'rvt_campo_ubicacion', 'Ubicación', array( $this, 'campo_ubicacion_html'), 'revisar-texto', 'rvt_seccion_a' );
+			register_setting( 'rvt_grupo_ubicacion', 'rvt_campo_ubicacion', array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'default' => '0'
+		) );
 	}
-	function ajustes_html()
+	function campo_ubicacion_html()
 	{
-		ob_start();
+		//ob_start();
+		?>
+		<div class="wrap">
+			<select name="rvt_grupo_ubicacion">
+				<option value="0">Al principio de la entrada</option>
+				<option value="1">Al final de la entrada</option>
+			</select>
+		</div>
+		<?php
+		//echo ob_get_clean();
+	}
+	function rvt_menu()
+	{
+		add_options_page( 'Ajustes Revisión de Texto', 'Revisar texto', 'manage_options', 'revisar-texto', array($this, 'rvt_pag_ajustes_html') );
+	}
+	function rvt_pag_ajustes_html()
+	{
+		//ob_start()
 		?>
 		<div class="wrap">
 			<h1>Hola</h1>
+			<form action="options.php" method="POST">
+				<?php
+				settings_fields( 'rvt_grupo_ubicacion' );
+				do_settings_sections( 'revisar-texto' );
+				submit_button();
+				?>
+			</form>
 		</div>
 		<?php
-		echo ob_get_clean();
+		//echo ob_get_clean();
 	}
 }
 
